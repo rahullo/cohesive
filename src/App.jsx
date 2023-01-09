c431cfe3ee40914607e6ca7c9bb185dc5d8ddce9
Rahul Lohra <rahullohra987@gmail.com>
2023-01-09 20:18:59 +0530

    Implementing routes

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import landingPage from './Component/landing.component/landing.component'
+
+function App() {
+  return <div className="App">
+    
+    My name is Rahul 
+    </div>
+}
+
+export default App
---
+++ src/Component/landing.component/landing.component.jsx
@@ -0,0 +1,7 @@
+function landingPage ()  {
+    return (
+        <div> My Name is Landing page..</div>
+    )
+}
+
+export default landingPage
